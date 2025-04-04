2dfe32afacfc9dde98e69c44617bf0564234caca
konarknangia <konarkrnangia@gmail.com>
2025-04-04 15:23:20 -0400

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,7 +1,10 @@
 #include "queue.h"
 #include "tile_game.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state) {
+  size_t val = serialize(state);
+  insert_at_tail(&q->list, val);
+}
 
 struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
 
