ea602ac1e7105891c0ff91b80c6935e2db2ff79f
konarknangia <konarkrnangia@gmail.com>
2025-04-04 16:26:38 -0400

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -65,7 +65,7 @@ size_t remove_from_tail(struct linked_list *list) {
 }
 
 void free_list(struct linked_list list) {
-  struct list_node *cur = list.head;
+  struct list_node *cur = list->head;
   while (cur != NULL) {
     struct list_node *next = cur->next;
     free(cur);
