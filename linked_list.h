e08890bba9642b708b76439fa113abbce9e29acc
konarknangia <konarkrnangia@gmail.com>
2025-04-04 16:41:20 -0400

    Update linked_list.h

--- linked_list.h
+++ linked_list.h
@@ -18,8 +18,8 @@ void insert_at_tail(struct linked_list *list, size_t value);
 size_t remove_from_head(struct linked_list *list);
 size_t remove_from_tail(struct linked_list *list);
 
-void dump_list(FILE *fp, struct linked_list list);
+void dump_list(FILE *fp, struct linked_list *list);
 
-void free_list(struct linked_list list);
+void free_list(struct linked_list *list);
 
 #endif // LINKED_LIST_H_
